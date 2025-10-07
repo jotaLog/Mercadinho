b783d94a3d53e3773d34a29ac3b1414e39ca67ff
jotaLog <joaopedrorochamendes86@gmail.com>
2025-10-07 20:57:13 -0300

    Inicio Mercadinho

---
+++ src/backend/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: ''
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error("Erro ao conectar ao banco de dados:", err);
+    } else {
+        console.log("Conexão ao banco de dados estabelecida com sucesso.");
+    }
+});
+
+module.exports = db;
